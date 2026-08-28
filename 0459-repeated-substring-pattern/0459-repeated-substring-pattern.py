0a9ab2ff938cbb5cb5f7d2cac87d51316c82911f
thas117 <thaslimajaleel8393@gmail.com>
2026-08-28 19:33:29 +0530

    Time: 5 ms (51.58%), Space: 12.6 MB (57.37%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def repeatedSubstringPattern(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        n = len(s)
+
+        for length in range(1, n // 2 + 1):
+            if n % length == 0:
+                substring = s[:length]
+
+                if substring * (n // length) == s:
+                    return True
+
+        return False        
