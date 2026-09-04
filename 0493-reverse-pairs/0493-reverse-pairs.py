cf5df1f7c89fc7d4734e90466bf333d7626ddc27
thas117 <thaslimajaleel8393@gmail.com>
2026-09-04 10:50:54 +0530

    Time: 1193 ms (68.99%), Space: 22.4 MB (39.72%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.py
@@ -0,0 +1,51 @@
+class Solution(object):
+    def reversePairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        def merge_sort(left, right):
+            if left >= right:
+                return 0
+
+            mid = (left + right) // 2
+
+            count = merge_sort(left, mid)
+            count += merge_sort(mid + 1, right)
+
+            # Count reverse pairs
+            j = mid + 1
+
+            for i in range(left, mid + 1):
+                while j <= right and nums[i] > 2 * nums[j]:
+                    j += 1
+
+                count += j - (mid + 1)
+
+            # Merge the two sorted halves
+            temp = []
+            i = left
+            j = mid + 1
+
+            while i <= mid and j <= right:
+                if nums[i] <= nums[j]:
+                    temp.append(nums[i])
+                    i += 1
+                else:
+                    temp.append(nums[j])
+                    j += 1
+
+            while i <= mid:
+                temp.append(nums[i])
+                i += 1
+
+            while j <= right:
+                temp.append(nums[j])
+                j += 1
+
+            nums[left:right + 1] = temp
+
+            return count
+
+        return merge_sort(0, len(nums) - 1)        
