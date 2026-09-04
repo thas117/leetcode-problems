477405b2aa9ed2e4afc55ce587d41866ab222f05
thas117 <thaslimajaleel8393@gmail.com>
2026-09-04 10:46:08 +0530

    Time: 15 ms (16.84%), Space: 13.3 MB (32.34%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        current_max = nums[0]
+        current_min = nums[0]
+        answer = nums[0]
+
+        for i in range(1, len(nums)):
+
+            num = nums[i]
+
+            # Negative number swaps max and min
+            if num < 0:
+                current_max, current_min = current_min, current_max
+
+            current_max = max(num, current_max * num)
+            current_min = min(num, current_min * num)
+
+            answer = max(answer, current_max)
+
+        return answer        
